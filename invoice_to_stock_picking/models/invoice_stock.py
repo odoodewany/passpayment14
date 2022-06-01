c25fa52eb5ef133fe1ae4c19eeafcbeb69f15178
VidalASM <vidalmsa@gmail.com>
2022-06-01 12:01:43 -0500

    action from view form/tree view from shipping shipment from invoice

--- invoice_to_stock_picking/models/invoice_stock.py
+++ invoice_to_stock_picking/models/invoice_stock.py
@@ -248,13 +248,18 @@ class InvoiceStockMove(models.Model):
 
 	def action_view_picking_shipment(self):
 		
-		action = self.env.ref('stock.action_picking_tree_all').read()[0]
+		action = self.env["ir.actions.actions"]._for_xml_id("stock.action_picking_tree_all") #self.env.ref('stock.action_picking_tree_all').read()[0]
 
 		pickings = self.env['stock.picking'].search([('origin', '=', self.name)])
 		if len(pickings) > 1:
 			action['domain'] = [('id', 'in', pickings.ids)]
 		elif pickings:
-			action['views'] = [(self.env.ref('stock.view_picking_form').id, 'form')]
+			# action['views'] = [(self.env.ref('stock.view_picking_form').id, 'form')]
+			form_view = [(self.env.ref('stock.view_picking_form').id, 'form')]
+			if 'views' in action:
+				action['views'] = form_view + [(state,view) for state,view in action['views'] if view != 'form']
+			else:
+				action['views'] = form_view
 			action['res_id'] = pickings.id
 		return action
 		pickings.state = "assigned"
