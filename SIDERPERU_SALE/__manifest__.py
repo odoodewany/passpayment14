7d58217ef350e57976dff7bee15b4b5590ffb2d3
horlando <orland20161@gmail.com>
2022-06-06 15:19:32 -0500

    add model SIDERPERU-SALE ,delete column impuestos from presupuesto/pedido

---
+++ SIDERPERU_SALE/__manifest__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+{
+    'name': 'Sales_siderperu',
+    'version': '14.0.3',
+    'category': 'Sales/Sales',
+    'summary': 'From quotations to invoices',
+    'website': 'https://www.odoo.com/page/sales',
+    'depends': ['sale', 'digest'],
+    'data': [
+        'views/sale.xml',
+    ],
+    'demo': [
+    ],
+    'application': False,
+    'auto_install': False,
+}
