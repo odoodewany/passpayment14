4a5c2f70d83d83c160855ac710f023d753c019e5
luis <l.a.vargas.molina@gmail.com>
2022-06-06 09:30:58 -0500

    Corregido reporte SIDER

--- summary_invoice_report/reports/account_move_summary_report.py
+++ summary_invoice_report/reports/account_move_summary_report.py
@@ -79,7 +79,7 @@ class SummaryAccountMoveLineReport(models.AbstractModel):
         sheet.merge_range(
             'A1:B3', self.env.user.company_id.name or "", format21_c_bold)
         sheet.merge_range(
-            'C1:P3', 'LINEAS', format21_c_bold)
+            'C1:R3', 'LINEAS', format21_c_bold)
 
         sheet.write('A4', 'Anulado', format21_left_bold)
         sheet.write('B4', 'Mes', format21_left_bold)
@@ -88,17 +88,21 @@ class SummaryAccountMoveLineReport(models.AbstractModel):
         sheet.write('E4', 'Tipo de documento', format21_left_bold)
         sheet.write('F4', 'Nro de documento del cliente',
                     format21_left_bold)
-        sheet.write(
-            'G4', 'Razón social del documento del cliente', format21_left_bold)
-        sheet.write('H4', 'Condición pago', format21_left_bold)
-        sheet.write('I4', 'Moneda', format21_left_bold)
-        sheet.write('J4', 'Código referencia', format21_left_bold)
-        sheet.write('K4', 'Producto/Servicio', format21_left_bold)
-        sheet.write('L4', 'Cantidad', format21_left_bold)
-        sheet.write('M4', 'Precio unitario', format21_left_bold)
-        sheet.write('N4', 'Subtotal', format21_left_bold)
-        sheet.write('O4', 'IGV', format21_left_bold)
-        sheet.write('P4', 'Total', format21_left_bold)
+        sheet.write('G4', 'Fecha de documento',
+                    format21_left_bold)
+        sheet.write('H4', 'Nro de documento del cliente',
+                    format21_left_bold)
+        sheet.write('I4', 'Razón social del cliente',
+                    format21_left_bold)
+        sheet.write('J4', 'Condición pago', format21_left_bold)
+        sheet.write('K4', 'Moneda', format21_left_bold)
+        sheet.write('L4', 'Código referencia', format21_left_bold)
+        sheet.write('M4', 'Producto/Servicio', format21_left_bold)
+        sheet.write('N4', 'Cantidad', format21_left_bold)
+        sheet.write('O4', 'Precio unitario', format21_left_bold)
+        sheet.write('P4', 'Subtotal', format21_left_bold)
+        sheet.write('Q4', 'IGV', format21_left_bold)
+        sheet.write('R4', 'Total', format21_left_bold)
 
         for summ in lines:
             for row, line in enumerate(summ.move_line_ids, 4):
@@ -111,35 +115,38 @@ class SummaryAccountMoveLineReport(models.AbstractModel):
                 document_type = line.partner_id.l10n_latam_identification_type_id.name if line.partner_id.l10n_latam_identification_type_id else ''
                 vat = line.partner_id.vat if line.partner_id.vat else ''
                 partner = line.partner_id.name if line.partner_id.name else ''
-                print (line.move_id.line_ids.filtered(lambda line: line.tax_line_id))
-                print (line.tax_line_id)
-                untaxed_amount = taxed_amount = 0
-                if not line.exclude_from_invoice_tab:
-                    untaxed_amount = line.amount_currency
-                elif line.tax_line_id:
-                    taxed_amount = line.amount_currency
+                invoice_document_type = line.move_id.move_type
+                invoice_document_number = line.move_id.name
+                invoice_date = line.move_id.invoice_date.strftime(
+                    '%d/%m/%Y') if line.move_id.invoice_date else ''
                 sheet.write(row, 0, state, format21_left)
                 sheet.write(row, 1, invoice_date_month,
                             format21_left)
                 sheet.write(row, 2, invoice_date_year,
                             format21_left)
                 sheet.write(row, 3, invoice_user, format21_left)
-                sheet.write(row, 4, document_type,
+                sheet.write(row, 4, invoice_document_type,
                             format21_left)
-                sheet.write(row, 5, vat,
+                sheet.write(row, 5, invoice_document_number,
                             format21_left)
-                sheet.write(row, 6, partner,
+                sheet.write(row, 6, invoice_date,
                             format21_left)
-                sheet.write(row, 7, line.move_id.invoice_payment_term_id.name,
+                sheet.write(row, 7, document_type,
                             format21_left)
-                sheet.write(row, 8, line.move_id.currency_id.name,
+                sheet.write(row, 8, vat,
                             format21_left)
-                sheet.write(row, 9, line.product_id.default_code,
+                sheet.write(row, 9, partner,
                             format21_left)
-                sheet.write(row, 10, line.product_id.name, format21_left)
-                sheet.write(row, 11, line.quantity, format21_left)
-                sheet.write(row, 12, line.price_unit, format21_left)
-                sheet.write(row, 13, line.price_subtotal, format21_left)
-                sheet.write(row, 14, line.price_total - line.price_subtotal, format21_left)
-                sheet.write(row, 15, line.price_total, format21_left)
-
+                sheet.write(row, 10, line.move_id.payment_reference,
+                            format21_left)
+                sheet.write(row, 11, line.move_id.currency_id.name,
+                            format21_left)
+                sheet.write(row, 12, line.product_id.default_code,
+                            format21_left)
+                sheet.write(row, 13, line.product_id.name, format21_left)
+                sheet.write(row, 14, line.quantity, format21_left)
+                sheet.write(row, 15, line.price_unit, format21_left)
+                sheet.write(row, 16, line.price_subtotal, format21_left)
+                sheet.write(row, 17, line.price_total -
+                            line.price_subtotal, format21_left)
+                sheet.write(row, 18, line.price_total, format21_left)
